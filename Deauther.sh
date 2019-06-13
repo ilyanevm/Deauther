b0d1b556c3447de382e4798afc8e5d98f793b321
ilyanevm <51771288+ilyanevm@users.noreply.github.com>
2019-06-13 13:21:19 +1000

    Create Deauther.sh

---
+++ Deauther.sh
@@ -0,0 +1,52 @@
+pt install figlet
+
+apt install termux-api
+
+echo 'STARTING DOWNLOAD SYSTEM LIBRARIES'
+
+
+figlet DEAUTHER
+
+termux-volume call 5
+termux-volume system 15
+termux-volume music 15
+
+termux-toast ГОТОВСЯ БЛЯ
+
+termux-torch on
+
+termux-tts-speak леха пидор
+
+termux-torch off
+
+termux-tts-speak леха пидор
+
+termux-torch on
+
+termux-tts-speak леха пидор
+
+termux-torch off
+
+termux-tts-speak леха пидор
+
+termux-torch on
+
+termux-tts-speak леха пидор
+
+termux-torch off
+
+termux-tts-speak леха пидор
+
+termux-torch on
+
+termux-tts-speak леха пидор
+
+termux-torch off
+
+termux-tts-speak леха пидор
+
+termux-torch on
+
+termux-tts-speak леха пидор
+
+termux-torch off
